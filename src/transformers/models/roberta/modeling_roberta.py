Fix attention mask creation if None

src/transformers/models/roberta/modeling_roberta.py
@@ -1177,18 +1177,17 @@ class RobertaModel(RobertaPreTrainedModel):
             past_key_values[0][0].shape[2] if past_key_values is not None else 0
         )
 
-        if attention_mask is None:
-            if self._use_flash_attention_2:
-                # 2d mask is passsed through the layers
-                attention_mask = (
-                    attention_mask
-                    if (attention_mask is not None and 0 in attention_mask)
-                    else None
-                )
-            else:
+        if getattr(self.config, "_flash_attn_2_enabled", False):
+            attention_mask = (
+                attention_mask
+                if (attention_mask is not None and 0 in attention_mask)
+                else None
+            )
+        else:
+            if attention_mask is None:
                 attention_mask = torch.ones(
-                    ((batch_size, seq_length + past_key_values_length)), device=device
-                )
+                    input_shape, device=device
+                )  # (bs, seq_length)
 
         if token_type_ids is None:
             if hasattr(self.embeddings, "token_type_ids"):